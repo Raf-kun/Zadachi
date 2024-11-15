dac6ced9a39dcbe702bbc4bd0736799af85026d3
Raf-kun <14katosik03@gmail.com>
2024-11-15 21:45:01 +0300

    Create zd9.py
    2.9

---
+++ zd9.py
@@ -0,0 +1,28 @@
+x = int(input("Введите месяц года (1-12): "))
+n = (x - 1) + 2
+print(n)
+
+if n == 1:
+    print("Январь.")
+if n == 2:
+    print("Февраль.")
+if n == 3:
+    print("Март.")
+if n == 4:
+    print("Апрель.")
+if n == 5:
+    print("Май.")
+if n == 6:
+    print("Июнь.")
+if n == 7:
+    print("Июль.")
+if n == 8:
+    print("Август.")
+if n == 9:
+    print("Сентябрь.")
+if n == 10:
+    print("Октябрь.")
+if n == 11:
+    print("Ноябрь.")
+if n == 12:
+    print("Декабрь.")
